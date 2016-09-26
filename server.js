9d72c8343190907364f0564ecaa1069c33cded27
reshigoutam <goutam1803@gmail.com>
2016-09-26 22:53:44 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/article-three', function(req, res){
     res.send('Article three requested and will be served here');
 });
 app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+  res.sendFile(path.join(__dirname, 'ui','main.js'));
 })
 
 app.get('/ui/madi.png', function (req, res) {
