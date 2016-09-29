31cd439c7dd2be0838aac2227e10e45d6bce70ae
reshigoutam <goutam1803@gmail.com>
2016-09-29 13:16:19 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,17 @@ console.log('Loaded!');
 var element = document.getElementById("main-text");
 
 element.innerHTML = "New Value";
+
+// move the image
+var img = document.getElementById("madi");
+var marginleft = 0;
+function moveRight () {
+    marginleft = marginleft + 10;
+    img.style.marginleft = marginleft + "px";
+}
+img.onclick = function() {
+    var interval = setInterval(moveRight, 100px);
+};
+
+
+
